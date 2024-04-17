f3c7ec81a2067d3716140610f5d8815741f5b25a
unknown <tommyandjerry1330@gmail.com>
2024-04-17 19:47:12 +0530

    created file main.java

---
+++ main.java
@@ -0,0 +1,9 @@
+class main{
+    public static void main(String args[]) {
+        System.out.println("i love java");
+        System.out.println("it is so interesting");
+        System.out.println("should complete learning it soon");
+
+    }
+
+}
